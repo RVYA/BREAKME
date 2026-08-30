2f63ef1465540ce3039069ef93db887f09fe5e09
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-30 22:23:09 +0300

    feat: Add `getTileEffectFrom` to the Tiles repo

--- src/repos/tiles.ts
+++ src/repos/tiles.ts
@@ -1,7 +1,10 @@
+import type TileEffectName from "#const/tiles/effects"
+import { TILE_EFFECTS_BY_NAME } from "#const/tiles/effects"
 import type TileShapeName from "#const/tiles/shapes"
 import { TILE_SHAPES_BY_NAME } from "#const/tiles/shapes"
 import type TileVariantName from "#const/tiles/variants"
 import { TILE_VARIANTS_BY_NAME } from "#const/tiles/variants"
+import type TileEffect from "#models/tile/effect"
 import type TileShape from "#models/tile/shape"
 import type TileVariant from "#models/tile/variant"
 
@@ -12,4 +15,9 @@ export function getTileShapeFrom(name: TileShapeName): TileShape {
 export function getTileVariantFrom(name?: TileVariantName): TileVariant | undefined {
 	if (!name) return undefined
 	else return TILE_VARIANTS_BY_NAME[name]
+}
+
+export function getTileEffectFrom(name?: TileEffectName): TileEffect | undefined {
+	if (!name) return undefined
+	else return TILE_EFFECTS_BY_NAME[name]
 }
