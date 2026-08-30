51266e31b12a17dc8da7818b32e0a3c7761787d2
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-30 22:19:16 +0300

    feat: Add `getTileVariantFrom` func to Tiles repo

--- src/repos/tiles.ts
+++ src/repos/tiles.ts
@@ -1,7 +1,15 @@
-import type TileShapeName from "../constants/tiles/tile-shapes"
-import { TILE_SHAPES_BY_NAME } from "../constants/tiles/tile-shapes"
-import type TileShape from "../models/tile/tile-shape"
+import type TileShapeName from "#const/tiles/shapes"
+import { TILE_SHAPES_BY_NAME } from "#const/tiles/shapes"
+import type TileVariantName from "#const/tiles/variants"
+import { TILE_VARIANTS_BY_NAME } from "#const/tiles/variants"
+import type TileShape from "#models/tile/shape"
+import type TileVariant from "#models/tile/variant"
 
 export function getTileShapeFrom(name: TileShapeName): TileShape {
 	return TILE_SHAPES_BY_NAME[name]
+}
+
+export function getTileVariantFrom(name?: TileVariantName): TileVariant | undefined {
+	if (!name) return undefined
+	else return TILE_VARIANTS_BY_NAME[name]
 }
