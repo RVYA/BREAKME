1bdb6af242a92ac0c0941949122735c5de520996
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-31 18:45:15 +0300

    refactor: Remove name based query functinos from Achievement and Collectible repos (empty for now)

--- src/repos/achievements.ts
+++ src/repos/achievements.ts
@@ -1,7 +1,1 @@
-import { ACHIEVEMENTS_BY_NAME } from "#const/unlockables/achievements"
-import type Achievement from "#models/unlockable/achievement"
-import type { AchievementName } from "#models/unlockable/achievement"
-
-export function getAchievementFrom(name: AchievementName): Achievement {
-	return ACHIEVEMENTS_BY_NAME[name]
-}
+export {}
--- src/repos/collectibles.ts
+++ src/repos/collectibles.ts
@@ -1,7 +1,1 @@
-import { COLLECTIBLES_BY_NAME } from "#const/unlockables/collectibles"
-import type Collectible from "#models/unlockable/collectible"
-import type { CollectibleName } from "#models/unlockable/collectible"
-
-export function getCollectibleFrom(name: CollectibleName): Collectible {
-	return COLLECTIBLES_BY_NAME[name]
-}
+export {}
