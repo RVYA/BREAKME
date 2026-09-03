b4f397bf66603a0385c952213476be0ac59184f2
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-03 21:44:54 +0300

    refactor: Rename data/action-items.ts to data/action-events.ts

---
+++ src/data/action-events.ts
@@ -0,0 +1,8 @@
+import type { ActionSource } from "#types/action-event"
+
+const ACTION_EVENTS = {
+	Git: ["commit", "mergeCommit", "branchCreate", "tagCreate"],
+	GitHub: ["pullRequest", "issue", "release", "deployment"],
+} as const satisfies Record<ActionSource, string[]>
+
+export { ACTION_EVENTS }
