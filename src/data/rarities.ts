f1fd2a7c0013f5ce8ada78ef4fa3ab287990621a
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-03 23:38:18 +0300

    refactor: Reorder rarities in ascending order.

--- src/data/rarities.ts
+++ src/data/rarities.ts
@@ -1,9 +1,9 @@
 import type { Rarity } from "#types/rarity"
 
 export const RARITY_DISTRIBUTION = [
-	{ label: "common", threshold: 1.0 },
-	{ label: "uncommon", threshold: 0.4 },
-	{ label: "rare", threshold: 0.15 },
-	{ label: "epic", threshold: 0.05 },
-	{ label: "legendary", threshold: 0.01 },
+	{ label: "legendary", threshold: 0.01 }, // 1%
+	{ label: "epic", threshold: 0.05 }, // 4%
+	{ label: "rare", threshold: 0.15 }, // 10%
+	{ label: "uncommon", threshold: 0.4 }, // 25%
+	{ label: "common", threshold: 1.0 }, // 60%
 ] as const satisfies Rarity[]
