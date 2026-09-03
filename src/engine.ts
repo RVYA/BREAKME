029a36f52157f31ba3b6836e75bdd57efcba18e1
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-04 00:51:52 +0300

    feat: Implement Engine class with initialization phase of the game

--- src/engine.ts
+++ src/engine.ts
@@ -1,1 +1,42 @@
-export {}
+import { loadOrInitializeState } from "#state/manager"
+import type GameState from "#types/game-state"
+
+type EngineOptions = {
+	username: string
+	userId?: string
+	statePath?: string
+	token?: string
+}
+
+export default class Engine {
+	#options: EngineOptions
+	#state?: GameState
+	#isInitialRun = false
+
+	constructor(options: EngineOptions) {
+		this.#options = options
+	}
+
+	get state(): GameState | undefined {
+		return this.#state
+	}
+
+	get isInitialRun(): boolean {
+		return this.#isInitialRun
+	}
+
+	async init(): Promise<GameState> {
+		const result = await loadOrInitializeState(
+			{
+				username: this.#options.username,
+				uuid: this.#options.userId,
+			},
+			this.#options.statePath,
+		)
+		this.#state = result.state
+		this.#isInitialRun = result.isInitialRun
+		return this.#state
+	}
+}
+
+export type { EngineOptions }
