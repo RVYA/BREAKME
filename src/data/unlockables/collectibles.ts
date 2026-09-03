9a58eb7723ce919dd36c875260f6e4c36166970d
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-03 21:33:24 +0300

    refactor: Separate Unlockable entities, group them under data/unlockables

---
+++ src/data/unlockables/collectibles.ts
@@ -0,0 +1,22 @@
+import type Collectible from "#types/unlockables/collectible"
+
+export const COLLECTIBLES = [
+	{
+		name: "Test Collectible #1",
+		description: "Let's see if everything works.",
+		rarity: "common",
+		spawnCondition: undefined,
+	},
+	{
+		name: "Test Collectible #2",
+		description: "Let's see if everything *extra* works.",
+		rarity: "common",
+		spawnCondition: undefined,
+	},
+	{
+		name: "Test Collectible #3",
+		description: "Ya da ya da.",
+		rarity: "uncommon",
+		spawnCondition: { minTileIndex: 2 },
+	},
+] as const satisfies Collectible[]
