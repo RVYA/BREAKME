6060dbb45ec54681c1974588bf94fa6549a8baa1
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-03 21:17:35 +0300

    chore: Remove redundant project files, restructure the project

--- src/models/rarity.ts
+++
@@ -1,7 +0,0 @@
-type RarityLabel = "common" | "uncommon" | "rare" | "epic" | "legendary"
-export type { RarityLabel as default }
-
-export type Rarity = {
-	label: RarityLabel
-	spawnRate: number
-}
