7c349d4c5226ae416dc0d0a080228b50e245e9a5
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-05 16:20:51 +0300

    feat: Add models for `CollectibleName`, derived from collectibles map keys, `CollectiblePool` model; update Collectible model to remove name as a prop, and add a symbol prop to be shown on trophy table

--- src/types/collectible.ts
+++ src/types/collectible.ts
@@ -1,8 +1,13 @@
+import type { COLLECTIBLES } from "#data/collectibles"
 import type { ActionType } from "#types/action-event"
 import type RarityLabel from "#types/rarity"
 import type { TileEffectName } from "#types/tile/effect"
 import type { TileShapeName } from "#types/tile/shape"
 import type { TileVariantName } from "#types/tile/variant"
+
+export type CollectibleName = keyof typeof COLLECTIBLES
+
+export type CollectiblePool = Partial<Record<RarityLabel, CollectibleName[]>>
 
 type SpawnCondition = {
 	applicableShapes?: TileShapeName[]
@@ -14,10 +19,11 @@ type SpawnCondition = {
 }
 
 type Collectible = {
-	name: string
+	symbol: string
 	description: string
 	rarity: RarityLabel
 	spawnCondition?: SpawnCondition
 }
 
-export type { Collectible as default, SpawnCondition }
+export type { Collectible as default, Collectible, SpawnCondition }
+
