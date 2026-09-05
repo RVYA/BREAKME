5c231bca6ddc7e1c327f2fe5b554c3169de1c2e4
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-05 16:19:40 +0300

    refactor: Update COLLECTIBLES map to enforce changes to `Collectible` model; extract collectible names as map keys, and add symbols to test collectibles

--- src/data/collectibles.ts
+++ src/data/collectibles.ts
@@ -1,22 +1,32 @@
 import type Collectible from "#types/collectible"
+import type { CollectibleName } from "#types/collectible"
+import type RarityLabel from "#types/rarity"
 
-export const COLLECTIBLES = [
-	{
-		name: "Test Collectible #1",
+export const COLLECTIBLES = {
+	"Test Collectible #1": {
+		symbol: "📦",
 		description: "Let's see if everything works.",
 		rarity: "common",
 		spawnCondition: undefined,
 	},
-	{
-		name: "Test Collectible #2",
+	"Test Collectible #2": {
+		symbol: "💎",
 		description: "Let's see if everything *extra* works.",
 		rarity: "common",
 		spawnCondition: undefined,
 	},
-	{
-		name: "Test Collectible #3",
+	"Test Collectible #3": {
+		symbol: "🏆",
 		description: "Ya da ya da.",
 		rarity: "uncommon",
 		spawnCondition: { minTileIndex: 2 },
 	},
-] as const satisfies Collectible[]
+} as const satisfies Record<string, Collectible>
+
+export const COLLECTIBLES_BY_RARITY = (Object.entries(COLLECTIBLES) as [CollectibleName, Collectible][]).reduce(
+	(acc, [name, item]) => {
+		;(acc[item.rarity] ??= []).push(name)
+		return acc
+	},
+	{} as Record<RarityLabel, CollectibleName[]>,
+)
